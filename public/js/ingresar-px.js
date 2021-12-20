db629aed1161d71c1be3e395f34a09f4e0c8eb44
Randy Sum <randy26smc@gmail.com>
2021-12-20 12:57:34 -0600

    Se ingresa un paciente nuevo y se verifica que todos los campos esten disponibles

--- public/js/ingresar-px.js
+++ public/js/ingresar-px.js
@@ -3,13 +3,25 @@ console.log("Registro de personas");
 function registrarPaciente(){
     //console.log("Hola bienvenido usuario que se quiere registrar");
     const registro = document.getElementById('registro-px');
+    let CUI = registro['CUI'];
     let nombre = registro['nombre'];
-    let usuario = registro['usuario']
-    let correoElectronico = registro['correoElectronico'];
-    let constrasenia = registro['contrasenia'];
-    let confContrasenia = registro['contraseniaConfirmar']
+    let apellido= registro['apellido']
+    let edad = registro['edad'];
+    let fechaNacimiento = registro['fechaNacimiento'];
+    let genero = registro['genero'];
+    let direccion = registro['direccion'];
+    
 
-    console.log('Bienvenido ' + nombre.value + " nuevo Usuario");
+
+    if((CUI.value == '') ||(nombre.value == '') || (apellido.value == '') || (edad.value == '') ||
+     (fechaNacimiento.value == '') || (genero.value == '') ||(direccion.value == '')){
+        console.log('no se pudo crear al usuario')
+    }
+    else{
+        console.log('Usuario creado con exito!');
+        console.log('Bienvenido: ' +nombre.value + ' ' + apellido.value);
+    }
+
 /*
     if(contrasenia.value == confContrasenia.value){
         console.log("Bienvenido, usuario creado correctamente");
